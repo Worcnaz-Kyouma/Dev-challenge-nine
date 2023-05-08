dd68323040cfb5b545245b407803cba944886dea
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-05-08 19:15:48 -0300

    validations with html in component NewPatient.jsx/component NewPatient.jsx logic implementation with react-query

--- client-side/src/components/NewPatient.jsx
+++ client-side/src/components/NewPatient.jsx
@@ -1,65 +1,106 @@
-import './styles/InputPatient.css'
+import { useMutation } from '@tanstack/react-query'
+import './styles/InputForm.css'
 
 export default function NewPatient(){
+    const patientMutation = useMutation({
+        mutationFn: (newPatient) => {
+            return fetch("http://localhost:22194/patients", {
+                    method: "POST",
+                    body: JSON.stringify(newPatient),
+                    headers: {"Content-type": "application/json; charset=UTF-8"}
+                }).then((res) => res.json())
+        },
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()
+    
+        const form = event.target
+        const formData = new FormData(form)
+
+        const formJson = Object.fromEntries(formData.entries())
+    
+        patientMutation.mutate(formJson)
+    }
+    
     return(
-        <form>
-            <div className="input-wrapper">
-                <input type="text" id="nmPatient" name="nmPatient" placeholder=' '/>
-                <label htmlFor="nmPatient">Nome</label>
-            </div>
+        <>
+            {patientMutation.data?.error && 
+            /*<ErrorMessage erroresponse={patientMutation.data.error} /> */
+            <p id='error'>
+                {patientMutation.data.error.type == 'UniqueConstraintError' && patientMutation.data.error.field.includes('dsEmail')
+                    ? 'This email is already in use!'
+                    :  patientMutation.data.error.message
+                }
+            </p>
+            }
 
-            <div className="input-wrapper">
-                <input type="date" id="dtBorn" name="dtBorn" placeholder=' '/>
-                <label htmlFor="dtBorn">Data de nascimento</label>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="input-wrapper">
+                    <input type="text" id="nmPatient" name="nmPatient" placeholder=' ' required/>
+                    <label htmlFor="nmPatient">Nome</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="email" id="dsEmail" name="dsEmail" placeholder=' '/>
-                <label htmlFor="dsEmail">Email</label>
-            </div>
-            
+                <div className="input-wrapper">
+                    <input type="date" id="dtBorn" name="dtBorn" placeholder=' ' required max={
+                        (() => {
+                            let date = new Date();
+                            date.setDate(date.getDate() + 1)
+                    
+                            return date.toJSON().slice(0,10)
+                        })()
+                    }/>
+                    <label htmlFor="dtBorn">Data de nascimento</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="text" id="nmCountry" name="nmCountry" placeholder=' '/>
-                <label htmlFor="nmCountry">Pais</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="email" id="dsEmail" name="dsEmail" placeholder=' ' required/>
+                    <label htmlFor="dsEmail">Email</label>
+                </div>
+                
 
-            <div className="input-wrapper">
-                <input type="text" id="nmCity" name="nmCity" placeholder=' '/>
-                <label htmlFor="nmCity">Cidade</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="text" id="nmCountry" name="nmCountry" placeholder=' ' required/>
+                    <label htmlFor="nmCountry">Pais</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="text" id="nmDistrict" name="nmDistrict" placeholder=' '/>
-                <label htmlFor="nmDistrict">Bairro</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="text" id="nmCity" name="nmCity" placeholder=' '/>
+                    <label htmlFor="nmCity">Cidade</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="text" id="dsAddress" name="dsAddress" placeholder=' '/>
-                <label htmlFor="dsAddress">Endereço</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="text" id="nmDistrict" name="nmDistrict" placeholder=' '/>
+                    <label htmlFor="nmDistrict">Bairro</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="number" id="nrAddress" name="nrAddress" placeholder=' '/>
-                <label htmlFor="nrAddress">Numero</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="text" id="dsAddress" name="dsAddress" placeholder=' ' required/>
+                    <label htmlFor="dsAddress">Endereço</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="text" id="dsComplement" name="dsComplement" placeholder=' '/>
-                <label htmlFor="dsComplement">Complemento</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="number" id="nrAddress" name="nrAddress" placeholder=' '/>
+                    <label htmlFor="nrAddress">Numero</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="text" id="cdCep" name="cdCep" placeholder=' '/>
-                <label htmlFor="cdCep">Cep</label>
-            </div>
+                <div className="input-wrapper">
+                    <input type="text" id="dsComplement" name="dsComplement" placeholder=' '/>
+                    <label htmlFor="dsComplement">Complemento</label>
+                </div>
 
-            <div className="input-wrapper">
-                <input type="text" id="cdUf" name="cdUf" placeholder=' '/>
-                <label htmlFor="cdUf">UF</label>
-            </div>
-            
-           <button type="submit" value="Submit">Submit</button>
-        </form>
+                <div className="input-wrapper">
+                    <input type="text" id="cdCep" name="cdCep" placeholder=' '/>
+                    <label htmlFor="cdCep">Cep</label>
+                </div>
+
+                <div className="input-wrapper">
+                    <input type="text" id="cdUf" name="cdUf" placeholder=' '/>
+                    <label htmlFor="cdUf">UF</label>
+                </div>
+                
+            <button type="submit" value="Submit">Submit</button>
+            </form>
+        </>
     )
 }
--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './components/Home'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Home />
-  //</React.StrictMode>,
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 )
