4a805d676b62ca26b790b85a532f37e48ae9b183
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-05-09 19:16:40 -0300

    InputForm.css to NewEditPatient.css / implementation of NewPatient.jsx and EditPatient.jsx form style / general styles improve

--- client-side/src/components/NewPatient.jsx
+++ client-side/src/components/NewPatient.jsx
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 import ErrorMessage from './ErrorMessage'
-import './styles/InputForm.css'
+import './styles/NewEditPatient.css'
 
 export default function NewPatient(){
     const patientMutation = useMutation({
@@ -28,71 +28,71 @@ export default function NewPatient(){
         <>
             {patientMutation.data?.error && <ErrorMessage errorResponse={patientMutation.data.error} />}
 
+            <h1 className='form-title'>Dados pessoais</h1>
+
             <form onSubmit={handleSubmit}>
-                <div className="input-wrapper">
-                    <input type="text" id="nmPatient" name="nmPatient" placeholder=' ' required/>
-                    <label htmlFor="nmPatient">Nome</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="date" id="dtBorn" name="dtBorn" placeholder=' ' required max={
-                        (() => {
-                            let date = new Date();
-                            date.setDate(date.getDate() + 1)
+                <div className='personal-data'>
+                    <div id='nmPatient-wrapper' className="input-wrapper">
+                        <input type="text" id="nmPatient" name="nmPatient" placeholder=' ' required/>
+                        <label htmlFor="nmPatient">Nome</label>
+                    </div>
+                    <div id='dtBorn-wrapper' className="input-wrapper">
+                        <input type="date" id="dtBorn" name="dtBorn" placeholder=' ' required max={
+                            (() => {
+                                let date = new Date();
+                                date.setDate(date.getDate() + 1)
                     
-                            return date.toJSON().slice(0,10)
-                        })()
-                    }/>
-                    <label htmlFor="dtBorn">Data de nascimento</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="email" id="dsEmail" name="dsEmail" placeholder=' ' required/>
-                    <label htmlFor="dsEmail">Email</label>
-                </div>
-                
-
-                <div className="input-wrapper">
-                    <input type="text" id="nmCountry" name="nmCountry" placeholder=' ' required/>
-                    <label htmlFor="nmCountry">Pais</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="text" id="nmCity" name="nmCity" placeholder=' '/>
-                    <label htmlFor="nmCity">Cidade</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="text" id="nmDistrict" name="nmDistrict" placeholder=' '/>
-                    <label htmlFor="nmDistrict">Bairro</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="text" id="dsAddress" name="dsAddress" placeholder=' ' required/>
-                    <label htmlFor="dsAddress">Endereço</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="number" id="nrAddress" name="nrAddress" placeholder=' '/>
-                    <label htmlFor="nrAddress">Numero</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="text" id="dsComplement" name="dsComplement" placeholder=' '/>
-                    <label htmlFor="dsComplement">Complemento</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="text" id="cdCep" name="cdCep" placeholder=' '/>
-                    <label htmlFor="cdCep">Cep</label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input type="text" id="cdUf" name="cdUf" placeholder=' '/>
-                    <label htmlFor="cdUf">UF</label>
+                                return date.toJSON().slice(0,10)
+                            })()
+                        }/>
+                        <label htmlFor="dtBorn">Data de nascimento</label>
+                    </div>
+                    <div id='dsEmail-wrapper' className="input-wrapper">
+                        <input type="email" id="dsEmail" name="dsEmail" placeholder=' ' required/>
+                        <label htmlFor="dsEmail">Email</label>
+                    </div>
                 </div>
                 
-            <button type="submit" value="Submit">Submit</button>
+
+                <div className='location-data'>
+                    <div id='nmCountry-wrapper' className="input-wrapper">
+                        <input type="text" id="nmCountry" name="nmCountry" placeholder=' ' required/>
+                        <label htmlFor="nmCountry">Pais</label>
+                    </div>
+                    <div id='nmCity-wrapper' className="input-wrapper">
+                        <input type="text" id="nmCity" name="nmCity" placeholder=' '/>
+                        <label htmlFor="nmCity">Cidade</label>
+                    </div>
+                    <div id='nmDistrict-wrapper' className="input-wrapper">
+                        <input type="text" id="nmDistrict" name="nmDistrict" placeholder=' '/>
+                        <label htmlFor="nmDistrict">Bairro</label>
+                    </div>
+                    <div id='dsAddress-wrapper' className="input-wrapper">
+                        <input type="text" id="dsAddress" name="dsAddress" placeholder=' ' required/>
+                        <label htmlFor="dsAddress">Endereço</label>
+                    </div>
+                    <div id='nrAddress-wrapper' className="input-wrapper">
+                        <input type="number" id="nrAddress" name="nrAddress" placeholder=' '/>
+                        <label htmlFor="nrAddress">Numero</label>
+                    </div>
+                    <div id='dsComplement-wrapper' className="input-wrapper">
+                        <input type="text" id="dsComplement" name="dsComplement" placeholder=' '/>
+                        <label htmlFor="dsComplement">Complemento</label>
+                    </div>
+                    <div id='cdCep-wrapper' className="input-wrapper">
+                        <input type="text" id="cdCep" name="cdCep" placeholder=' '/>
+                        <label htmlFor="cdCep">Cep</label>
+                    </div>
+                    <div id='cdUf-wrapper' className="input-wrapper">
+                        <input type="text" id="cdUf" name="cdUf" placeholder=' '/>
+                        <label htmlFor="cdUf">UF</label>
+                    </div>
+                </div>
+                
+                <button type="submit" value="Submit" id='submit-btn' className={
+                    (patientMutation.isSuccess && !patientMutation.data?.error)
+                        && 'sucess'
+                }>Submit</button>
             </form>
         </>
     )
