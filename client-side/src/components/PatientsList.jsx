24142ebde4b15d8f5446ffc47f536b345a1eeabf
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-05-11 17:23:42 -0300

    start PatientsList.jsx component development

--- client-side/src/components/PatientsList.jsx
+++ client-side/src/components/PatientsList.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react'
 import PatientRow from './PatientRow'
+import { useQuery } from '@tanstack/react-query'
 
 export default function PatientsList(){
+    const [ patients, setPatients ] = useState(null)
+    const [ page, setPage ] = useState(1)
+    const [ nmPatientSearched, setNmPatientSearched ] = useState(null)
+
+    const patientQuery = useQuery({
+        queryKey: ['patients', { page: page }, { nmPatient: nmPatientSearched }],
+        queryFn: () => {
+            return fetch("http://localhost:22194/patients?" + "limit=5" + "&" + "page=" + page + nmPatientSearched && ("&" + "nmPatient=" + nmPatientSearched))
+            .then((res) => res.json())
+            .then((resJson) => {
+                if(resJson?.error)
+                    throw resJson.error
+                return resJson
+            })
+        },
+        staleTime:  30 * 1000
+    })
     return(
         <>
             <PatientRow />
