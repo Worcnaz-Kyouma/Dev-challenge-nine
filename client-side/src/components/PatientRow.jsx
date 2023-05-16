9b3f1e241d81758e787a126c0ef5922229578fd8
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-05-16 19:46:19 -0300

    PatientRow.jsx implemented

--- client-side/src/components/PatientRow.jsx
+++ client-side/src/components/PatientRow.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react"
 
 import './styles/PatientRow.css'
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export default function PatientRow(props){
+    const queryClient = useQueryClient()
+
+    const patientMutation = useMutation({
+        mutationFn: () => {
+            return fetch("http://localhost:22194/patients/" + props.patient.pkIdPatient, {
+                method: "DELETE"
+            })
+        },
+        onSuccess: () => queryClient.invalidateQueries(["patients"])
+    })
+
     const [ deleteMode, setDeleteMode ] = useState(false)
+
     return(
-            <tr className="patient-row" onClick={props.selectPatient}>
-                {!deleteMode
-                    ?   <>
+                !deleteMode
+                    ?   <tr className="patient-row" onClick={props.selectPatient}>
                             <td>{props.patient.nmPatient}</td>
                             <td>{props.patient.dsEmail}</td>
                             <td>{new Intl.DateTimeFormat('pt-BR', { day: 'numeric', month: 'long', year: 'numeric'} ).format(new Date(props.patient.dtBorn))}</td>
@@ -15,21 +27,26 @@ export default function PatientRow(props){
                                 e.stopPropagation()
                                 setDeleteMode(true)
                             }}>Delete</button></td>
-                        </>
-                    :   <div className="delete">
-                            <span>Do you really wanna delete {props.patient.nmPatient}?</span>
-                            <button onClick={(e) => {
-                                e.stopPropagation()
-                                patientMutation.mutate()//delete!
-                            }}>Yes!</button>
-                            <button onClick={(e) => {
-                                //setTimeout(() => setDeleteMode(false), 2000)? Para dar um intervalo de 2 segundos para sumirmos com estes dados de maneira chad com css?
-                                e.stopPropagation()
-                                setDeleteMode(false)
-                            }}>No</button>
-                        </div>
-                }
-            </tr>
+                        </tr>
+
+                    :   <tr className="patient-delete">
+                            <td colSpan={'100%'}>
+                                <div>
+                                    <span>Do you really wanna delete {props.patient.nmPatient}?</span>
+                                    <button onClick={(e) => {
+                                        e.stopPropagation()
+                                        patientMutation.mutate()//delete!
+                                    }}>Yes!</button>
+                                    <button onClick={(e) => {
+                                        //setTimeout(() => setDeleteMode(false), 2000)? Para dar um intervalo de 2 segundos para sumirmos com estes dados de maneira chad com css?
+                                        e.stopPropagation()
+                                        setDeleteMode(false)
+                                    }}>No</button>
+                                </div>
+                            </td>
+                        </tr>
+                
+            
         
     )
 }
